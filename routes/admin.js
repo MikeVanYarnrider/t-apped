ba90aa5e64d774d8ec9c194f9f16604ec5119c67
Michael Garnreiter <mgarnreiter@aol.com>
2019-11-17 17:51:50 +0100

    add user and admin routes

--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-/* GET home page */
+// GET admin dashboard
 router.get("/", (req, res, next) => {
-  res.render("index");
+  res.render("admin/errorDashboard.hbs");
 });
 
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -1,13 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const passport = require("passport");
-const User = require("../models/User");
 
-// user GET dashboard
-router.get("/user/:id", (req, res, next) => {
-  console.log(req.params.id);
-  User.findById(req.params.id);
-  res.render("/user/dashboard.hbs");
+// GET user dashboard
+router.get("/", (req, res, next) => {
+  res.render("user/dashboard.hbs");
 });
 
 module.exports = router;

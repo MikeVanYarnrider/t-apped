57b6f6b31f99b3ed72166f06d61af6e8f34a0616
Michael Garnreiter <mgarnreiter@aol.com>
2019-11-17 18:08:49 +0100

    add login check that only logged in users can see the user dashboard

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const router = express.Router();
 
+const loginCheck = () => {
+  return (req, res, next) => {
+    if (req.user) {
+      next();
+    } else {
+      res.redirect("/");
+    }
+  };
+};
+
 // GET user dashboard
-router.get("/", (req, res, next) => {
+router.get("/", loginCheck(), (req, res, next) => {
   res.render("user/dashboard.hbs");
 });
 

1ef3c3449eeddb93d93c7f6c52a3127f173e76ca
Michael Garnreiter <mgarnreiter@aol.com>
2019-11-17 17:55:28 +0100

    deleted admin model and implemented role type in user schema

--- models/User.js
+++ models/User.js
@@ -1,15 +1,22 @@
-const mongoose = require('mongoose');
-const Schema   = mongoose.Schema;
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
-const userSchema = new Schema({
-  username: String,
-  password: String
-}, {
-  timestamps: {
-    createdAt: 'created_at',
-    updatedAt: 'updated_at'
+const userSchema = new Schema(
+  {
+    username: String,
+    password: String
+  },
+  {
+    timestamps: {
+      createdAt: "created_at",
+      updatedAt: "updated_at"
+    },
+    role: {
+      type: String,
+      enum: ["user", "admin"]
+    }
   }
-});
+);
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 module.exports = User;

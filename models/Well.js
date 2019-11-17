8eb4f59c0700eb7051993575faad0323820bd4f9
Michael Garnreiter <mgarnreiter@aol.com>
2019-11-17 15:52:47 +0100

    add comments/rating/images to wellSchema

--- models/Well.js
+++ models/Well.js
@@ -5,10 +5,22 @@ const wellSchema = new Schema({
   name: String,
   type: String,
   coordinates: Object, //{lat:000, lng:000}
-  availability: String, //"open", "not available", "closed"
+  availability: {
+    type: String,
+    enum: ["open", "not available", "closed"],
+    default: "open"
+  },
   accessability: Boolean,
   noteworthy: Boolean,
-  inOperation: Object //{from:, to:, always: Bool}
+  inOperation: Object, //{from:, to:, always: Bool}
+  ratings: [Number],
+  imageUrls: [String]
+  comments: [
+    {
+    type: Schema.Types.ObjectId,
+    ref: "Comment"
+  }
+]
 });
 
 const Well = mongoose.model("Well", wellSchema);
